f4eedf90031860eaad186ab1a296f0b3fc5427ac
Andre B <limaufrpe289@gmail.com>
2021-07-30 16:42:37 -0300

    Refactoring codes

--- Estrutura-de-Dados-em-Java/Fila-Refatoracao-Generics/src/main/java/br/com/refatoracao/Fila.java
+++ Estrutura-de-Dados-em-Java/Fila-Refatoracao-Generics/src/main/java/br/com/refatoracao/Fila.java
@@ -1,2 +1,87 @@
-package br.com.refatoracao;public class Fila {
+package br.com.refatoracao;
+
+public class Fila<T> {
+    private No<T> refNoEntradaFila;
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+    }
+
+
+    //No embutido (tecnica de encapsulamento)
+    public void enqueue(T object){
+        No novoNo = new No(object);
+
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    //retornara o objeto e nao o No
+    public T first(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            while (true){
+                if (primeiroNo.getRefNo() != null){
+                    primeiroNo = primeiroNo.getRefNo();
+
+                }else{
+                    break;
+                }
+
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    //retornara o objeto e nao o No
+    public T dequeue(){
+        if(!this.isEmpty()){
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true){
+                if (primeiroNo.getRefNo() != null){
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+
+                }else{
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+
+            }
+            return (T) primeiroNo.getObject();
+
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return  refNoEntradaFila == null? true:false;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "--------------------\n";
+        stringRetorno += "    Fila   \n";
+        stringRetorno += "--------------------\n";
+
+        No noAuxiliar = refNoEntradaFila;
+        if (refNoEntradaFila != null){
+            while (true){
+                stringRetorno += "[No{objeto " + noAuxiliar.getObject() + "}]--->";
+                if (noAuxiliar.getRefNo() != null){
+                    noAuxiliar = noAuxiliar.getRefNo();
+                }else{
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+
+        }else{
+            stringRetorno = "null";
+        }
+
+        return stringRetorno;
+    }
 }
--- Estrutura-de-Dados-em-Java/Fila-Refatoracao-Generics/src/main/java/br/com/refatoracao/Main.java
+++ Estrutura-de-Dados-em-Java/Fila-Refatoracao-Generics/src/main/java/br/com/refatoracao/Main.java
@@ -1,2 +1,26 @@
-package br.com.refatoracao;public class Main {
+package br.com.refatoracao;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fila<String> minhaFila = new Fila<>();
+        minhaFila.enqueue("primeiro");
+        minhaFila.enqueue("segundo");
+        minhaFila.enqueue("terceiro");
+        minhaFila.enqueue("quarto");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+
+
+
+
+
+    }
 }
