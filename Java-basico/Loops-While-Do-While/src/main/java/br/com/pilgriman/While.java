624f8be589e0f7d5f93b80e3f5e9184c06a568ce
Andre B <limaufrpe289@gmail.com>
2021-08-14 12:33:58 -0300

    just practicing some loops

--- Java-basico/Loops-While-Do-While/src/main/java/br/com/pilgriman/While.java
+++ Java-basico/Loops-While-Do-While/src/main/java/br/com/pilgriman/While.java
@@ -1,2 +1,38 @@
-package br.com.pilgriman;public class While {
+package br.com.pilgriman;
+
+public class While {
+    public static void main(String[] args) {
+     /*int i = 1;
+     int max= 10;
+
+     System.out.println("Contando até " + max);
+     while (i <= 10){
+         System.out.println("O valor de i: " + i);
+         i++;
+     }*/
+
+
+     /*int i = 10;
+       int min = 1;
+        System.out.println("Contagem regressiva até " + min);
+       while(i >= min){
+           System.out.println("O valor de i: " + i);
+           i--;
+       }*/
+
+        //Do While
+        int i = 1;
+        int max = 15;
+        System.out.println("Contando até " + max);
+
+        do {
+            i++;
+            System.out.println("O valor de i: " + i);
+        } while (i <= max);
+
+
+
+
+        }
+
 }
