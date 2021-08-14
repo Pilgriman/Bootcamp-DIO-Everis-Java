19e54918614afd431382825dc69d4746f848667d
Andre B <limaufrpe289@gmail.com>
2021-08-14 12:32:10 -0300

    a simple exercise about switchCase

--- Java-basico/Controle-de-Decisao-SwitchCase/src/main/java/br/com/pilgriman/SwitchCase.java
+++ Java-basico/Controle-de-Decisao-SwitchCase/src/main/java/br/com/pilgriman/SwitchCase.java
@@ -1,2 +1,57 @@
-package br.com.pilgriman;public class SwitchCase {
-}
+package br.com.pilgriman;
+
+import java.util.Scanner;
+//Até o java 7 a condição podia ser um byte, short, int, char ou Enum
+//com o java 7 em em diante a condição passa a receber strings também
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um numero que corresponde a um dia da semana(1-7):");
+
+        int diaSemana = scan.nextInt();
+
+        /*if (diaSemana == 1){
+            System.out.println("Domingo");
+        }else if (diaSemana == 2){
+            System.out.println("Segunda");
+
+        }else if ( diaSemana == 3){
+            System.out.println("Terça");
+        }else if (diaSemana == 4){
+            System.out.println("Quarta");
+        }else if (diaSemana == 5){
+            System.out.println("Quinta");
+        }else if (diaSemana == 6){
+            System.out.println("Sexta");
+        }else if (diaSemana == 7){
+            System.out.println("Sábado");
+        }else{
+            System.out.println("Não é um dia da semana válido");
+        }*/
+
+        switch (diaSemana){
+            case 1:
+            System.out.println("Domingo");break;
+            case 2:
+            System.out.println("Segunda");break;
+            case 3:
+            System.out.println("Terça");break;
+            case 4:
+            System.out.println("Quarta");break;
+            case 5:
+            System.out.println("Quinta");break;
+            case 6:
+            System.out.println("Sexta");break;
+            case 7:
+            System.out.println("Sábado");break;
+            default:
+                System.out.println("Não é um dia da semana válido");
+
+        }
+
+        }
+
+    }
+
